ca1a118422c6346bd11047ee7ffb997707edf4e3
fu01101010 <j0s3phv3rm33r@gmail.com>
2024-08-27 00:03:09 +0300

    added light and material headers

---
+++ source/graphics/light.cpp
@@ -0,0 +1,48 @@
+#include "light.h"
+
+void pointLight::render(shader Shader, int IDx) {
+
+	std::string name = "PointLights[" + std::to_string(IDx) + "]";
+
+	Shader.set3flt(name + ".position", position);
+
+	Shader.set_flt(name + ".k0", k0);
+	Shader.set_flt(name + ".k1", k1);
+	Shader.set_flt(name + ".k2", k2);
+
+	Shader.set3flt(name + ".ambient", ambient);
+	Shader.set3flt(name + ".diffuse", diffuse);
+	Shader.set3flt(name + ".specular", specular);
+}
+
+void DIRECTLIGHT::RENDER(SHADER Shader) {
+
+	std::string name = "DirectLight";
+
+	Shader.set3flt(name + ".direction", direction);
+
+	Shader.set3flt(name + ".ambient", ambient);
+	Shader.set3flt(name + ".diffuse", diffuse);
+	Shader.set3flt(name + ".specular", specular);
+}
+
+
+void SPOTLIGHT::RENDER(SHADER Shader, int IDx) {
+
+	std::string name = "SpotLights[" + std::to_string(IDx) + "]";
+	//std::string name = "SpotLight";
+
+	Shader.set3flt(name + ".position", position);
+	Shader.set3flt(name + ".direction", direction);
+	
+	Shader.set_flt(name + ".cutOff", cutOff);
+	Shader.set_flt(name + ".outerCutOff", outerCutOff);
+
+	Shader.set_flt(name + ".k0", k0);
+	Shader.set_flt(name + ".k1", k1);
+	Shader.set_flt(name + ".k2", k2);
+
+	Shader.set3flt(name + ".ambient", ambient);
+	Shader.set3flt(name + ".diffuse", diffuse);
+	Shader.set3flt(name + ".specular", specular);
+}
---
+++ source/graphics/light.h
@@ -0,0 +1,55 @@
+#ifndef LIGHT_H
+#define LIGHT_H
+
+#include <glm/glm.hpp>
+
+#include "shader.h"
+
+struct pointLight {
+
+	glm::vec3 position;
+
+	//attenuation constants
+	float k0;
+	float k1;
+	float k2;
+
+	glm::vec3 ambient;
+	glm::vec3 diffuse;
+	glm::vec3 specular;
+
+	void render(shader Shader, int IDx);
+};
+
+struct directLight {
+
+	glm::vec3 direction;
+
+	glm::vec3 ambient;
+	glm::vec3 diffuse;
+	glm::vec3 specular;
+
+	void render(shader Shader);
+};
+
+struct spotLight {
+
+	glm::vec3 position;
+	glm::vec3 direction;
+
+	float cutOff;
+	float outerCutOff;
+
+	//attenuation constants
+	float k0;
+	float k1;
+	float k2;
+
+	glm::vec3 ambient;
+	glm::vec3 diffuse;
+	glm::vec3 specular;
+
+	void render(shader Shader, int IDx);
+};
+
+#endif
