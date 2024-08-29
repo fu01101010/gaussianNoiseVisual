9e5a32a7088166f032a42948b76a80e2e7cc9535
fu01101010 <j0s3phv3rm33r@gmail.com>
2024-08-29 15:45:05 +0300

    added vCube and vLightsource

---
+++ source/graphics/models/vCube.hpp
@@ -0,0 +1,97 @@
+#ifndef VCUBE_HPP
+#define VCUBE_HPP
+
+#include "../vModel.h"
+#include "../material.h"
+
+class vCube : public vModel {
+
+public:
+
+	glm::vec3 position;
+	glm::vec3 size;
+
+	material Material;
+
+	vCube() {}
+	vCube(material Material, glm::vec3 position, glm::vec3 size) 
+		: Material(Material), position(position), size(size) {}
+
+	void init() {
+
+		unsigned int nVertices = 36;
+
+		float vertices[] = {
+			//position		normal
+			-0.5f, -0.5f, -0.5f,	0.0f,  0.0f, -1.0f,			 
+			 0.5f, -0.5f, -0.5f,	0.0f,  0.0f, -1.0f,
+			 0.5f,  0.5f, -0.5f,	0.0f,  0.0f, -1.0f,			 
+			 0.5f,  0.5f, -0.5f,	0.0f,  0.0f, -1.0f,
+ 			-0.5f,  0.5f, -0.5f,	0.0f,  0.0f, -1.0f,			
+			-0.5f, -0.5f, -0.5f,	0.0f,  0.0f, -1.0f,
+
+			-0.5f, -0.5f,  0.5f,     0.0f,  0.0f,  1.0f,
+			 0.5f, -0.5f,  0.5f,     0.0f,  0.0f,  1.0f,
+			 0.5f,  0.5f,  0.5f,     0.0f,  0.0f,  1.0f,
+			 0.5f,  0.5f,  0.5f,     0.0f,  0.0f,  1.0f,
+			-0.5f,  0.5f,  0.5f,     0.0f,  0.0f,  1.0f,
+			-0.5f, -0.5f,  0.5f,     0.0f,  0.0f,  1.0f,
+
+			-0.5f,  0.5f,  0.5f,    -1.0f,  0.0f,  0.0f,		
+			-0.5f,  0.5f, -0.5f,    -1.0f,  0.0f,  0.0f,	
+			-0.5f, -0.5f, -0.5f,    -1.0f,  0.0f,  0.0f,		
+			-0.5f, -0.5f, -0.5f,    -1.0f,  0.0f,  0.0f,		
+			-0.5f, -0.5f,  0.5f,    -1.0f,  0.0f,  0.0f,		
+			-0.5f,  0.5f,  0.5f,    -1.0f,  0.0f,  0.0f,
+
+			 0.5f,  0.5f,  0.5f,     1.0f,  0.0f,  0.0f,
+			 0.5f,  0.5f, -0.5f,     1.0f,  0.0f,  0.0f,
+			 0.5f, -0.5f, -0.5f,     1.0f,  0.0f,  0.0f,
+			 0.5f, -0.5f, -0.5f,     1.0f,  0.0f,  0.0f,
+			 0.5f, -0.5f,  0.5f,     1.0f,  0.0f,  0.0f,
+			 0.5f,  0.5f,  0.5f,     1.0f,  0.0f,  0.0f,
+
+			-0.5f, -0.5f, -0.5f,     0.0f, -1.0f,  0.0f,
+			 0.5f, -0.5f, -0.5f,     0.0f, -1.0f,  0.0f,
+			 0.5f, -0.5f,  0.5f,     0.0f, -1.0f,  0.0f,
+			 0.5f, -0.5f,  0.5f,     0.0f, -1.0f,  0.0f,
+			-0.5f, -0.5f,  0.5f,     0.0f, -1.0f,  0.0f,
+			-0.5f, -0.5f, -0.5f,     0.0f, -1.0f,  0.0f,
+
+			-0.5f,  0.5f, -0.5f,     0.0f,  1.0f,  0.0f,
+			 0.5f,  0.5f, -0.5f,     0.0f,  1.0f,  0.0f,
+			 0.5f,  0.5f,  0.5f,     0.0f,  1.0f,  0.0f,
+			 0.5f,  0.5f,  0.5f,     0.0f,  1.0f,  0.0f,
+			-0.5f,  0.5f,  0.5f,     0.0f,  1.0f,  0.0f,
+			-0.5f,  0.5f, -0.5f,     0.0f,  1.0f,  0.0f,
+		};
+
+		std::vector<unsigned int> indices(nVertices);
+
+		for (int i = 0; i < nVertices; ++i) {
+
+			indices[i] = i;
+		}
+
+		meshes.push_back(vMesh(vertex::genList(vertices, nVertices), indices));
+	}
+
+	void render(shader Shader) {
+
+		glm::mat4 model = glm::mat4(1.0f);
+
+		model = glm::translate(model, position);
+		model = glm::scale(model, size);
+		//model = glm::rotate(model, (float)glfwGetTime() * glm::radians(-45.0f), glm::vec3(0.5f));
+
+		Shader.setmat4("model", model);
+		Shader.set3flt("Material.ambient", Material.ambient);
+		//Shader.set3flt("Material.diffuse", Material.diffuse);
+		//Shader.set3flt("Material.specular", Material.specular);
+		Shader.set_flt("Material.reflectivity", Material.reflectivity);
+
+		vModel::render(Shader);
+	}
+};
+
+#endif
---
+++ source/graphics/models/vLightsource.hpp
@@ -0,0 +1,34 @@
+#ifndef VLIGHTSOURCE_HPP
+#define VLIGHTSOURCE_HPP
+
+#include "vCube.hpp"
+#include "../Light.h"
+
+class vLightsource : public vCube {
+
+public:
+
+	glm::vec3 lightColor;
+
+	//light strength
+	pointLight PointLight;
+
+	vLightsource() {}
+	vLightsource(
+		glm::vec3 lightColor, 
+		glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular, 
+		float k0, float k1, float k2,
+		glm::vec3 position, glm::vec3 size)
+		:	lightColor(lightColor), 
+			PointLight({position, k0, k1, k2, ambient, diffuse, specular}),
+			vCube(material::white_plastic, position, size) {}
+
+	void render(shader Shader) {
+		//SET LIGHT color
+		Shader.set3flt("lightColor", lightColor);
+
+		vCube::render(Shader);
+	}
+};
+
+#endif
